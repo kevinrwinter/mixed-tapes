aae75d7d497c428e5c7bc2404842485e71a2bd3b
Kevin Winter <kevin.winter@edu.bc.fi>
2022-04-27 14:08:49 +0300

    Setup - router, controller; install twig template; profiler -debugging

--- src/Controller/MixedTapesController.php
+++ src/Controller/MixedTapesController.php
@@ -3,10 +3,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
-class MixedTapesController {
+class MixedTapesController extends AbstractController {
 
   #[Route('/')]
   // #[Route('/home')]
@@ -14,7 +16,41 @@ class MixedTapesController {
   {
 
     // die("Mixed-Tapes: Surely, not fancy-looking page");
-    return new Response("Pink Floyd --- Another Brick In the wall");
+    
+    // return new Response("Pink Floyd --- Another Brick In the wall");
+
+    $tracks = [
+      ['song'=>'Gangsta\'s Paradise', 'artist'=> 'Coolio'],
+      ['song'=>'Waterfalls', 'artist'=> 'TLC'],
+      ['song'=>'Creep', 'artist'=> 'Radiohead'],
+      ['song'=>'Kiss From a Rose', 'artist'=> 'Seal'],
+      ['song'=>'On Bended Knee', 'artist'=> 'Boyz II Men'],
+      ['song'=>'Fantasy', 'artist'=> 'Mariah Carey'],
+    ];
+
+    // Debug:
+    // dd($tracks);
+    // Dev tools:
+    dump($tracks);
+    
+    return $this->render('mixed/homepage.html.twig', [
+      'title' => 'Mixed 90s music',
+      'tracks' => $tracks,
+    ]);
+  }
+
+  #[Route('/browse/{slug}')]
+  public function browse(string $slug = null): Response
+  {
+
+    if ($slug) {
+      $title = u(str_replace('-', '', $slug))->title(true);
+      
+    } else {
+      $title = "All Genres";
+    }
+    return new Response('Genre: ' .$title);
+    // return new Response("Breakup mixed-tape? Angsty 90s rock? Browse the collection!");
     
   }
   
